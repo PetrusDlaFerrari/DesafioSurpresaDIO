a9c97ddb84e5171fa20d23d6b091fdd3d713c5d1
Petrus Juannitto <139983618+PetrusDlaFerrari@users.noreply.github.com>
2024-02-20 20:05:04 -0300

    desafio.js

--- desafio.js
+++ desafio.js
@@ -3,4 +3,4 @@ let levelPokemon = ["level 2", "level 2", "level 5", "level 5", "level 5", "leve
 let genderPokemon = ["Masculino", "Feminino", "Masculino", "Feminino", "Feminino", "Feminino", "Masculino"]
 let lifePointsPokemon = ["13/13", "13/13", "25/25", "24/24", "24/24", "15/15", "7/14"]
 
-console.log ("Cadastrado com Sucesso " + pokemon[0])
+console.log (pokemon[] + " Cadastrado com Sucesso")
