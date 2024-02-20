117d115b4a04c0f4aa4a9c177f8a496c23a95d8d
Petrus Juannitto <139983618+PetrusDlaFerrari@users.noreply.github.com>
2024-02-20 19:42:48 -0300

    desafio.js

---
+++ desafio.js
@@ -0,0 +1,6 @@
+let pokemon = ["Poochyena", "Zigzagoon", "Dragonite", "Dragonite", "Dragonite", "Poochyena", "Wurmple"]
+let levelPokemon = ["level 2", "level 2", "level 5", "level 5", "level 5", "level 3", "level 2"]
+let genderPokemon = ["Masculino", "Feminino", "Masculino", "Feminino", "Feminino", "Feminino", "Masculino"]
+let lifePointsPokemon = ["13/13", "13/13", "25/25", "24/24", "24/24", "15/15", "7/14"]
+
+console.log ("Cadastrado com Sucesso " + pokemon[0])
